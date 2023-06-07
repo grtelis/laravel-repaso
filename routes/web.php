7fbb1fba8f1b72b68a709bfe36ef335b5a4f13cf
grtelis <elizabeth_torres@comunidad.unam.mx>
2023-06-07 14:24:03 -0600

    views

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,25 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return 'Ruta home';
+    return view('home');
 });
 
+// Route::get('/', function () {
+//     return 'Ruta home';
+// });
+
 Route::get('blog', function () {
-    return 'Listado de publicaciones';
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+    return view('blog', ['posts' => $posts]);
 });
 
 Route::get('blog/{slug}', function ($slug) {
     // consulta en base de datos
-
-    return $slug;
+    $post = $slug;
+    return view('post', ['post' => $post]);
 });
 
 Route::get('buscar', function (Request $request) {
