6abbfd077c37123bd19f630fd2bfda8092743dbe
grtelis <elizabeth_torres@comunidad.unam.mx>
2023-06-12 00:36:37 -0600

    validacionesé

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,9 +21,15 @@ class PostController extends Controller
 
     public function store(Request $request) {
 
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:posts,slug',
+            'body' => 'required'
+        ]);
+
         $request->user()->posts()->create([
-            'title' => $title = $request->title,
-            'slug' => Str::slug($title),
+            'title' => $request->title,
+            'slug' => $request->slug,
             'body' => $request->body
         ]);
         return redirect()->route('posts.index');
@@ -32,6 +38,22 @@ class PostController extends Controller
     public function edit(Post $post) {
         // return view('posts.edit', ['post' => $post]);
         return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post) {
+
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:posts,slug,' . $post->id,
+            'body' => 'required'
+        ]);
+
+        $post->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' => $request->body
+        ]);
+        return redirect()->route('posts.index');
     }
 
     public function destroy(Post $post) {
